d3c5a96606480868f38efefb12502f28935d1a25
llogen <christophlange95@googlemail.com>
2021-07-14 15:42:54 +0200

    update the token handling

--- pkg/client/github/githubAPI.go
+++ pkg/client/github/githubAPI.go
@@ -2,9 +2,11 @@ package githubAPI
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/url"
+	"os"
 	"regexp"
 
 	"github.com/google/go-github/github"
@@ -13,13 +15,23 @@ import (
 
 func EditGithubStatus(ctx context.Context, state string, targeturl string, description string, sha string) error {
 	//setting up the github authentication
+	f, err := os.Open("githubtoken.json")
+	if err != nil {
+		fmt.Printf("Could no open the github token file!\n")
+	}
+	defer f.Close()
+	tok := &oauth2.Token{}
+	err = json.NewDecoder(f).Decode(tok)
+	if err != nil {
+		fmt.Printf("Could not decode the json file")
+	}
 	ts := oauth2.StaticTokenSource(
-		&oauth2.Token{AccessToken: "YOURGITHUBTOKEN"},
+		&oauth2.Token{AccessToken: tok.AccessToken},
 	)
 	tc := oauth2.NewClient(ctx, ts)
 	client := github.NewClient(tc)
 
-	_, err := url.ParseRequestURI(targeturl)
+	_, err = url.ParseRequestURI(targeturl)
 	if err != nil {
 		return fmt.Errorf("TargetURL of the results is not formatted right! GithubStatus could not be edited")
 	}
@@ -30,9 +42,9 @@ func EditGithubStatus(ctx context.Context, state string, targeturl string, descr
 		return err
 	}
 	input := &github.RepoStatus{State: &state, TargetURL: &targeturl, Context: &description}
-	_, _, erro := client.Repositories.CreateStatus(ctx, "llogen", "webhook", sha, input)
-	if erro != nil {
-		log.Printf("could not set status of the commit to %s: err=%s\n", state, erro)
+	_, _, err = client.Repositories.CreateStatus(ctx, "llogen", "webhook", sha, input)
+	if err != nil {
+		log.Printf("could not set status of the commit to %s: err=%s\n", state, err)
 	}
 	return nil
 }
