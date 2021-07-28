18a73bdfa1269f91c16b8c07521938862184700d
llogen <christophlange95@googlemail.com>
2021-07-28 17:18:55 +0200

    contestcli/webhookhandler: added more comments for better reading.
    Signed-off-by: llogen <christophlange95@googlemail.com>

--- contestcli/webhookhandler.go
+++ contestcli/webhookhandler.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/google/go-github/github"
 )
@@ -20,30 +21,40 @@ func webhook(webhookData chan []string) {
 	log.Fatal(http.ListenAndServeTLS("0.0.0.0:6000", "/certs/fullchain.crt", "/certs/server.key", nil))
 }
 
-//handleWebhook handles the incoming webhooks and then adapt the jobDescriptor templates to kick off new jobs depending on them
+//handleWebhook handles incoming webhooks
 func (channel *Channel) handleWebhook(w http.ResponseWriter, r *http.Request) {
-	var tmp []string //declare variable to pass data through a channel
+	//declare variable to pass data through a channel
+	var tmp []string
 
-	payload, err := github.ValidatePayload(r, []byte("thisisasecret")) //receiving and validating the incoming webhook
+	//retrieve the github_secret for the webhook from .env
+	github_secret := os.Getenv("GITHUB_SECRET")
+	//receiving and validating the incoming webhook
+	payload, err := github.ValidatePayload(r, []byte(github_secret))
 	if err != nil {
 		log.Printf("error reading request body: err=%s\n", err)
 		return
 	}
 	defer r.Body.Close()
-	event, err := github.ParseWebHook(github.WebHookType(r), payload) //parsing the incoming webhook
+
+	//parsing the incoming webhook
+	event, err := github.ParseWebHook(github.WebHookType(r), payload)
 	if err != nil {
 		log.Printf("could not parse incoming webhook: %v\n", err)
 		return
 	}
-	switch e := event.(type) { //switch to handle the different eventtypes of the webhook
+
+	//switch to handle the different eventtypes of the webhook
+	switch e := event.(type) {
 	case *github.PullRequestEvent:
 		if *e.Action == "synchronize" || *e.Action == "closed" {
 			return
 		}
 		fmt.Printf("successful received pullrequest event\n")
+		//add, 1.the HEAD SHA, 2.the SSH link, 3.the Reference SHA, to tmp and than pass it to the channel
 		tmp = append(tmp, *e.PullRequest.Head.SHA, *e.PullRequest.Head.Repo.SSHURL, *e.PullRequest.Head.Ref)
 		channel.webhookdata <- tmp
 	case *github.PushEvent:
+		//add, 1.the commmit SHA, 2.the SSH link, to tmp and than pass it to the channel
 		fmt.Printf("successful received push event\n")
 		tmp = append(tmp, *e.After, *e.Repo.SSHURL)
 		channel.webhookdata <- tmp
