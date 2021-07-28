b901108a5aaf07fd068f9980f65fc28651d044d3
llogen <christophlange95@googlemail.com>
2021-07-28 16:59:19 +0200

    contestcli/contestcli: adding more comments/ make the code cleaner
    Signed-off-by: llogen <christophlange95@googlemail.com>

--- contestcli/contestcli.go
+++ contestcli/contestcli.go
@@ -19,21 +19,18 @@ import (
 	flag "github.com/spf13/pflag"
 )
 
+//define flags
 var (
 	flagSet    *flag.FlagSet
 	flagConfig *string
-	flagStates *[]string
-	flagTags   *[]string
 )
 
+//init the flags
 func initFlags(cmd string) {
 	flagSet = flag.NewFlagSet(cmd, flag.ContinueOnError)
 	flagConfig = flagSet.StringP("config", "c", "clientconfig.json", "Path to the configuration file that describes the client")
 
-	// Flags for the "list" command.
-	flagStates = flagSet.StringSlice("states", []string{}, "List of job states for the list command. A job must be in any of the specified states to match.")
-	flagTags = flagSet.StringSlice("tags", []string{}, "List of tags for the list command. A job must have all the tags to match.")
-
+	// define flag usage
 	flagSet.Usage = func() {
 		fmt.Fprintf(flagSet.Output(),
 			`Usage:
@@ -41,22 +38,8 @@ func initFlags(cmd string) {
   contestcli [flags] command
 
 Commands:
-  start [file]
-        start a new job using the job description from the specified file
-        or passed via stdin.
-        when used with -wait flag, stdout will have two JSON outputs
-        for job start and completion status separated with newline
-  stop int
-        stop a job by job ID
-  status int
-        get the status of a job by job ID
-  retry int
-        retry a job by job ID
-  list [--states=JobStateStarted,...] [--tags=foo,...]
-        list jobs by state and/or tags
   version
         request the API version to the server
-
 Flags:
 `)
 		flagSet.PrintDefaults()
@@ -64,6 +47,7 @@ Flags:
 }
 
 func CLIMain(cmd string, args []string, stdout io.Writer) error {
+	//init the flags
 	initFlags(cmd)
 	if err := flagSet.Parse(args); err != nil {
 		if err == flag.ErrHelp {
@@ -72,20 +56,22 @@ func CLIMain(cmd string, args []string, stdout io.Writer) error {
 		return err
 	}
 
-	//Open the configfile
+	//open the configfile
 	configFile, err := os.Open(*flagConfig)
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer configFile.Close()
-	//parse and decode the json file
+
+	//parse and decode the json configfile
 	configDescription, _ := ioutil.ReadAll(configFile)
 	var cd client.ClientDescriptor
 	if err := json.Unmarshal([]byte(configDescription), &cd); err != nil {
 		fmt.Printf("unable to decode the config file with err: %v\n", err)
 		fmt.Printf("content is: %+v\n", cd)
 	}
-	//Setting defaults to empty config entries
+
+	//setting defaults to empty config entries
 	if *cd.Flags.FlagAddr == "" {
 		*cd.Flags.FlagAddr = "http://localhost:8080"
 	}
@@ -96,39 +82,48 @@ func CLIMain(cmd string, args []string, stdout io.Writer) error {
 		*cd.Flags.FlagLogLevel = "debug"
 	}
 
+	//create logLevel
 	logLevel, err := logger.ParseLogLevel(*cd.Flags.FlagLogLevel)
 	if err != nil {
 		return err
 	}
 
+	//create context
 	ctx, cancel := xcontext.WithCancel(logrusctx.NewContext(logLevel, logging.DefaultOptions()...))
-
 	defer cancel()
 
+	//initialize the plugin registry
 	clientPluginRegistry := clientpluginregistry.NewClientPluginRegistry(ctx)
 	clientplugins.Init(clientPluginRegistry, ctx.Logger())
 
 	if err := cd.Validate(); err != nil {
 		return err
 	}
-	//creating a channel
+
+	//creating a channel with a buffer size of 10, it's big enough
 	webhookData := make(chan []string, 10)
-	//starting go routine
+
+	//starting go routine to run a webhooklistener
 	go webhook(webhookData)
 
+	//go over all PreJobExecutionHooks for every webhook run it and than go over all PostJobExecutionHooks
 	for nextWebhookData := range webhookData {
 		for _, eh := range cd.PreJobExecutionHooks {
+			//validate the current plugin
 			if err := eh.PreValidate(); err != nil {
 				return err
 			}
+			//register the current plugin
 			bundlePreExecutionHook, err := clientPluginRegistry.NewPreJobExecutionHookBundle(ctx, eh)
 			if err != nil {
 				return err
 			}
+			//run the plugin
 			if _, err = bundlePreExecutionHook.PreJobExecutionHooks.Run(ctx, bundlePreExecutionHook.Parameters, cd, &http.HTTP{Addr: *cd.Flags.FlagAddr}, nextWebhookData); err != nil {
 				return err
 			}
 		}
+		//run the job and receive the
 		var rundata map[int][2]string
 		rundata, err := run(ctx, cd, &http.HTTP{Addr: *cd.Flags.FlagAddr}, stdout, nextWebhookData)
 		if err != nil {
@@ -136,13 +131,16 @@ func CLIMain(cmd string, args []string, stdout io.Writer) error {
 		}
 
 		for _, eh := range cd.PostJobExecutionHooks {
+			//validate the current plugin
 			if err := eh.PostValidate(); err != nil {
 				return err
 			}
+			//register the current plugin
 			bundlePostExecutionHook, err := clientPluginRegistry.NewPostJobExecutionHookBundle(ctx, eh)
 			if err != nil {
 				return err
 			}
+			//run the plugin
 			if _, err := bundlePostExecutionHook.PostJobExecutionHooks.Run(ctx, bundlePostExecutionHook.Parameters, cd, &http.HTTP{Addr: *cd.Flags.FlagAddr}, rundata); err != nil {
 				return err
 			}
