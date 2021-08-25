39cc1a7e6d702d44cf4cb5149f0da3d57bdad287
llogen <christophlange95@googlemail.com>
2021-08-25 13:07:35 +0200

    contestcli/verbs: Removed all strings and added error handling instead. Also did some minor improvements.
    Signed-off-by: llogen <christophlange95@googlemail.com>

--- contestcli/verbs.go
+++ contestcli/verbs.go
@@ -8,9 +8,9 @@ import (
 	"html/template"
 	"io"
 	"io/ioutil"
-	"log"
 	"net/http"
 	"path/filepath"
+	"strings"
 
 	"github.com/9elements/contest-client/pkg/client"
 	githubAPI "github.com/9elements/contest-client/pkg/github"
@@ -34,87 +34,91 @@ func run(ctx context.Context, cd client.ClientDescriptor, transport transport.Tr
 
 	// Iterate over all JobTemplates that are defined in the clientconfig.json
 	for i := 0; i < len(cd.Flags.FlagJobTemplate); i++ {
-		// Open the configfile
-		filepath, _ := filepath.Abs("descriptors/")
-		filepathtemplate := filepath + "/" + *cd.Flags.FlagJobTemplate[i]
-		// Parse and decode the json/yaml file
-		templateDescription, err := ioutil.ReadFile(filepathtemplate)
+
+		// Create Path to the jobTemplate
+		filePath, _ := filepath.Abs("descriptors/")
+		filePathTemplate := strings.Join([]string{filePath, *cd.Flags.FlagJobTemplate[i]}, "/")
+
+		// Parse the json/yaml file
+		templateDescription, err := ioutil.ReadFile(filePathTemplate)
 		if err != nil {
-			log.Printf("could not parse the jobtemplate: %s\n", err)
+			return nil, fmt.Errorf("could not parse the jobtemplate: %w", err)
 		}
 
 		// Retrieve the jobName for further usages
 		jobName, err := RetrieveJobName(templateDescription, *cd.Flags.FlagYAML)
 		if err != nil {
-			fmt.Printf("could not retrieve the job name: %+v\n", err)
+			return nil, fmt.Errorf("could not retrieve the job name: %w", err)
 		}
+
 		// Adapt the jobDescriptor based on the webhookdata
 		jobDesc, err := ChangeJobDescriptor(templateDescription, webhookData)
 		if err != nil {
-			fmt.Printf("could not change the job template: %+v\n", err)
+			return nil, fmt.Errorf("could not change the job template: %w", err)
 		}
+
 		// If template file is YAML convert it to JSON
 		if *cd.Flags.FlagYAML {
 			// Unmarshal the data in a map
 			var body interface{}
-			if err := yaml.Unmarshal(jobDesc, &body); err != nil {
-				fmt.Printf("failed to parse YAML job descriptor: %+v", err)
+			err := yaml.Unmarshal(jobDesc, &body)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse YAML job descriptor: %w", err)
 			}
 			body = dyno.ConvertMapI2MapS(body)
 			// then marshal the structure back to JSON
 			jobDesc, err = json.MarshalIndent(body, "", "    ")
 			if err != nil {
-				fmt.Printf("failed to serialize job descriptor to JSON: %+v", err)
+				return nil, fmt.Errorf("failed to serialize job descriptor to JSON: %w", err)
 			}
 		}
 
 		// Updating the github status to pending
-		res := githubAPI.EditGithubStatus(ctx, "pending", "",
-			jobName+". Test-Result:", webhookData.headSHA)
-		if res != nil {
-			log.Printf("could not change the github status: %s\n", res)
+		err = githubAPI.EditGithubStatus(ctx, "pending", "", jobName+". Test-Result:", webhookData.headSHA)
+		if err != nil {
+			return nil, fmt.Errorf("could not change the github status: %w", err)
 		}
 
 		// Kick off the generated Job
 		startResp, err := transport.Start(context.Background(), *cd.Flags.FlagRequestor, string(jobDesc))
 		if err != nil {
-			fmt.Printf("could not send the Job with the jobDesc: %s\n", *cd.Flags.FlagJobTemplate[i])
-			err := githubAPI.EditGithubStatus(ctx, "error", "", jobName+". Test-Result:", webhookData.headSHA)
-			if err != nil {
-				fmt.Println("GithubStatus could not be edited to status: error", err)
+			// If the server is not reachable
+			statusErr := githubAPI.EditGithubStatus(ctx, "error", "", jobName+". Test-Result:", webhookData.headSHA)
+			if statusErr != nil {
+				return nil, fmt.Errorf("could not send the Job to the server: %w. GithubStatus also could not be edited to status: error", err)
 			}
-			return jobs, err
+			return nil, fmt.Errorf("could not send the Job to the server: %w", err)
 		} else {
+			// If the job could not executed
 			if int(startResp.Data.JobID) == 0 {
-				fmt.Printf("The Job could not executed. Server returned JobID 0! \n") //TODO: ERROR HANDLING
+				return nil, fmt.Errorf("the Job could not executed. Server returned JobID 0! %w", err)
 			}
 		}
 
 		// Filling the map with job data for postjobexecutionhooks
-		jobdata := client.RunData{JobName: jobName, JobSHA: webhookData.headSHA}
-		jobs[int(startResp.Data.JobID)] = jobdata
+		jobData := client.RunData{JobName: jobName, JobSHA: webhookData.headSHA}
+		jobs[int(startResp.Data.JobID)] = jobData
 
 		// Create Json Body for API Request to set a status for the started Job
 		data := map[string]interface{}{
 			"ID":     startResp.Data.JobID,
 			"Status": false,
 		}
-		json_data, err := json.Marshal(data) //Marshal that data
-		if err != nil {
-			fmt.Println("Could not parse data to json format.")
-		}
-		// Add the job to the Api DB
-		// TODO: Add the Address to the config!
-		addr := *cd.Flags.FlagAddr + *cd.Flags.FlagPortAPI + "/addjobstatus/"
-		resp, err := http.Post(addr, "application/json", bytes.NewBuffer(json_data))
 
+		// Marshal that data
+		jsonData, err := json.Marshal(data)
 		if err != nil {
-			fmt.Println("Could not post data to API.")
-			return nil, err
+			return nil, fmt.Errorf("could not parse data to json format: %w", err)
+		}
+
+		// Add the job to the Api DB
+		addr := strings.Join([]string{*cd.Flags.FlagAddr, *cd.Flags.FlagPortAPI, "/addjobstatus/"}, "")
+		resp, err := http.Post(addr, "application/json", bytes.NewBuffer(jsonData))
+		if err != nil {
+			return nil, fmt.Errorf("could not post data to API: %w", err)
 		}
 		if resp.StatusCode != 200 {
-			fmt.Println("The HTTP Post responded a statuscode != 200")
-			return nil, err
+			return nil, fmt.Errorf("the HTTP Post responded a statuscode != 200 %w", err)
 		}
 	}
 	return jobs, nil
@@ -124,11 +128,13 @@ func run(ctx context.Context, cd client.ClientDescriptor, transport transport.Tr
 func ChangeJobDescriptor(data []byte, webhookData WebhookData) ([]byte, error) {
 	// Create buffer to pass the adapted data
 	var buf bytes.Buffer
-	datastring := string(data)
+
+	// Convert data to a string that could be parsed
+	dataString := string(data)
 	// Create the data that should be substitute
 	jobDescData := templatedata{webhookData.headSHA}
 	// Parse the file data
-	tmpl, err := template.New("jobDesc").Delims("[[", "]]").Parse(datastring)
+	tmpl, err := template.New("jobDesc").Delims("[[", "]]").Parse(dataString)
 	if err != nil {
 		return buf.Bytes(), fmt.Errorf("parse the data for templates: %t", err)
 	}
@@ -143,25 +149,24 @@ func ChangeJobDescriptor(data []byte, webhookData WebhookData) ([]byte, error) {
 
 // Unmarshal the data from the template file and retrieve the jobName
 func RetrieveJobName(data []byte, YAML bool) (string, error) {
+	// String interface{} map to unmarshal the incoming data
 	var jobDesc map[string]interface{}
+
 	// Check if the file is YAML or JSON and unmarshal it
 	if !YAML {
 		// Retrieve the data and decode it into the jobDesc map
 		if err := json.Unmarshal(data, &jobDesc); err != nil {
 			return "", fmt.Errorf("failed to parse JSON job descriptor: %w", err)
 		}
-		// Retrieve the jobName from the JSON file
-		jobName := jobDesc["JobName"]
-		// Return the jobName
-		return jobName.(string), nil
 	} else {
 		// Retrieve the data and decode it into the jobDesc map
 		if err := yaml.Unmarshal(data, &jobDesc); err != nil {
 			return "", fmt.Errorf("failed to parse YAML job descriptor: %w", err)
 		}
-		// Retrieve the jobName from the YAML file
-		jobName := jobDesc["JobName"]
-		// Return the jobName
-		return jobName.(string), nil
 	}
+
+	// Retrieve the jobName from the JSON file
+	jobName := jobDesc["JobName"]
+	// Return the jobName
+	return jobName.(string), nil
 }
