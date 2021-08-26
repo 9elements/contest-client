281f2085ffc1eb4597388328e87ce9804000e59c
llogen <christophlange95@googlemail.com>
2021-08-26 13:19:34 +0200

    plugins/postjobexecutionhooks/pushtoS3: Reworked the complete pushtoS3 plugin. Adapted that the parameter are yield through the clientconfig.json file. Added the parameter validation. Adjusted the flow of the PushResultsToS3 function, which was a mess. Retrieving the binary url via a regex, which stil could improved. Also the job success will determined via a simple bool. The rest are minor or major adjustments.
    Signed-off-by: llogen <christophlange95@googlemail.com>

--- plugins/postjobexecutionhooks/pushtoS3/pushtoS3.go
+++ plugins/postjobexecutionhooks/pushtoS3/pushtoS3.go
@@ -2,7 +2,11 @@ package pushtoS3
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 
 	"github.com/9elements/contest-client/pkg/client"
 	"github.com/facebookincubator/contest/pkg/transport"
@@ -11,14 +15,79 @@ import (
 // Name defines the name of the preexecutionhook used within the plugin registry
 var Name = "pushtoS3"
 
-// Noop is a reporter that does nothing. Probably only useful for testing.
-type PushtoS3 struct{}
+// PushtoS3 uploads a file to a specific AWS S3 Bucket
+type PushtoS3 struct {
+	s3Region   string // Defines the S3 server region
+	s3Bucket   string // Defines the S3 bucket name
+	s3Path     string // Defines the S3 bucket upload path
+	awsFile    string // Defines the AWS config file location
+	awsProfile string // Defines the AWS config file profile
+}
 
 // ValidateRunParameters validates the parameters for the run reporter
 func (n *PushtoS3) ValidateParameters(params []byte) (interface{}, error) {
-	fmt.Println("I am validating the pushtoS3 parameters")
-	var s string
-	return s, nil
+	// Retrieve the parameter into PushtoS3 struct
+	var s3Param PushtoS3
+	err := json.Unmarshal(params, &s3Param)
+	if err != nil {
+		return nil, fmt.Errorf("pushToS3 could not unmarshal the parameter while validating them: %w", err)
+	}
+
+	// Validate the s3Region
+	if s3Param.s3Region == "" {
+		return nil, fmt.Errorf("s3Region cannot be empty: %w", err)
+	}
+	// Validate the s3Bucket
+	if s3Param.s3Bucket == "" {
+		return nil, fmt.Errorf("s3Bucket cannot be empty: %w", err)
+	}
+	// Validate the s3Path
+	if s3Param.s3Path == "" {
+		return nil, fmt.Errorf("s3Path cannot be empty: %w", err)
+	}
+
+	// Validate the awsFile
+	// If awsFile was not set to default
+	if s3Param.awsFile != "" {
+		err = validateAWS(s3Param.awsFile, s3Param.awsProfile)
+		if err != nil {
+			return nil, err
+		}
+		// If awsFile was set to default
+	} else if s3Param.awsFile == "" {
+		err = validateAWS("~/.aws/credentials", s3Param.awsProfile)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return s3Param, nil
+}
+
+func validateAWS(file string, awsProfile string) error {
+	// Open the awsFile and parse it as string
+	_, err := os.Stat(file)
+	if err != nil {
+		return fmt.Errorf("awsFile does not exist: %w", err)
+	}
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return fmt.Errorf("could not read the awsFile: %w", err)
+	}
+	fileString := string(data)
+
+	// Check if the given awsProfile exists or not
+	if awsProfile != "" {
+		exists := strings.Contains(fileString, awsProfile)
+		if !exists {
+			return fmt.Errorf("awsProfile does not exist!")
+		}
+	} else if awsProfile == "" {
+		exists := strings.Contains(fileString, "default")
+		if !exists {
+			return fmt.Errorf("default awsProfile does not exist!")
+		}
+	}
+	return nil
 }
 
 // Name returns the Name of the reporter
@@ -26,17 +95,25 @@ func (n *PushtoS3) Name() string {
 	return Name
 }
 
-// RunReport calculates the report to be associated with a job run.
+// Run invokes PushResultsToS3 for each job, which uploads the job result
+func (n *PushtoS3) Run(ctx context.Context, parameter interface{}, cd client.ClientDescriptor, transport transport.Transport,
+	rundata map[int]client.RunData) (interface{}, error) {
 
-func (n *PushtoS3) Run(ctx context.Context, parameters interface{}, cd client.ClientDescriptor, transport transport.Transport, rundata map[int]client.RunData) (interface{}, error) {
-	fmt.Println("Started the postjobplugin PushtoS3")
+	// Retrieving the parameter
+	var s3Param PushtoS3 = parameter.(PushtoS3)
+
+	// Iterate over the different jobs
 	for jobID, jobData := range rundata {
-		//retrieve the name of the job
+		// Retrieve the name of the job
 		jobName := jobData.JobName
-		//retrieve the SHA of the commit
+		// Retrieve the SHA of the commit
 		jobSHA := jobData.JobSHA
-		//kicking off the main logic in a go routine
-		go PushResultsToS3(ctx, cd, transport, jobName, jobSHA, jobID)
+
+		// Start the main logic of the plugin
+		err := PushResultsToS3(ctx, cd, transport, s3Param, jobName, jobSHA, jobID)
+		if err != nil {
+			return nil, fmt.Errorf("PushResultToS3 did not finished: %w", err)
+		}
 	}
 	return nil, nil
 }
--- plugins/postjobexecutionhooks/pushtoS3/s3.go
+++ plugins/postjobexecutionhooks/pushtoS3/s3.go
@@ -8,11 +8,12 @@ import (
 	"io/ioutil"
 	"net/http"
 	"regexp"
+	"strings"
 	"time"
 
 	"github.com/9elements/contest-client/pkg/client"
-	githubAPI "github.com/9elements/contest-client/pkg/github"
-	slackAPI "github.com/9elements/contest-client/pkg/slack"
+	"github.com/9elements/contest-client/pkg/githubAPI"
+	"github.com/9elements/contest-client/pkg/slackAPI"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -22,243 +23,163 @@ import (
 	"github.com/facebookincubator/contest/pkg/types"
 )
 
-// Define some constants for uploading things into a S3 bucket
-const (
-	S3_REGION   = "eu-central-1"
-	S3_BUCKET   = "coreboot-spr-sp-images"
-	S3_RESULTS  = "test_results"
-	S3_BINARIES = "binaries"
-)
-
-type url struct {
-	Msg string
-}
-
+// Function that upload the job result into a S3 bucket
 func PushResultsToS3(ctx context.Context, cd client.ClientDescriptor,
-	transport transport.Transport, jobName string, jobSha string, jobID int) error {
+	transport transport.Transport, parameter PushtoS3, jobName string, jobSha string, jobID int) error {
 
 	// Create a single AWS session (we can re use this if we're uploading many files)
-	s, err := session.NewSession(&aws.Config{Region: aws.String(S3_REGION),
+	s, err := session.NewSession(&aws.Config{Region: aws.String(parameter.s3Region),
 		Credentials: credentials.NewSharedCredentials(
-			"",           // file name
-			"9e-AWS-Key", // profile name
+			parameter.awsFile,    // awsfile name
+			parameter.awsProfile, // awsprofile name
 		)})
 	if err != nil {
-		return err
+		return fmt.Errorf("starting an aws session failed: %w", err)
 	}
 
 	// Creating link to read out the status of the running job from an api
-	readjobstatus := *cd.Flags.FlagAddr + *cd.Flags.FlagPortAPI + "/readjobstatus/" + fmt.Sprint(jobID)
+	readJobStatus := strings.Join([]string{*cd.Flags.FlagAddr, *cd.Flags.FlagPortAPI, "/readjobstatus/", fmt.Sprint(jobID)}, "")
 
+	// create a job.Report variable to store the job reports in further proceeding
 	var jobStatus [][]*job.Report
-	var binaryURL []job.TestStatus
 
 	// Loop til the job report is finished and uploaded
 	for {
-		// Api request
-		resp, err := http.Get(readjobstatus)
+		// API request
+		resp, err := http.Get(readJobStatus)
 		if err != nil {
-			fmt.Println("Could not post data to API.")
-			return err
+			return fmt.Errorf("could not post data to API: %w", err)
 		}
 		defer resp.Body.Close()
 
-		// If Api requst was successful
-		if resp.StatusCode == http.StatusOK {
-			var Status bool
-			bodyBytes, err := ioutil.ReadAll(resp.Body)
-			json.Unmarshal(bodyBytes, &Status)
-			if err != nil {
-				fmt.Println("Error reading the HTTP response", err)
-			}
-			// Status contains the job status (true = job finished, false = job still running)
-			// If the job is finished
-			if Status {
-				// Than retrieve the jobReport
-				resp, err := transport.Status(context.Background(), *cd.Flags.FlagRequestor, types.JobID(jobID))
-				if err != nil {
-					return err
-				}
-				// Decoding
-				respBodyBytes := new(bytes.Buffer)
-				json.NewEncoder(respBodyBytes).Encode(resp)
-
-				// Invoke function that uploads the test report to a S3 Bucket
-				// The function returns the name of the file that was uploaded to use it for the fileurl
-				filename, err := AddFileToS3(s, respBodyBytes.Bytes(), jobID)
-				if err != nil {
-					return err
-				}
-
-				// Creating link where the job report can be downloaded.
-				// This link will be put into the commit message right after the test status
-				fileurl := "https://" + S3_BUCKET + ".s3." + S3_REGION + ".amazonaws.com/" + filename
-
-				// Go through the report and retrieve the binaryURL from the uploadfile teststep
-				jobStatus = resp.Data.Status.JobReport.RunReports
-				binaryURL = resp.Data.Status.RunStatus.TestStatuses
-
-				// Go through all final reports
-				for _, finalreports := range jobStatus {
-					var matcherr bool = false
-					var matchsucceed bool = false
-					//Go through all report in the final reports
-					for _, reports := range finalreports {
-						var status = reports.Data
-						fmt.Println("reports: ", reports)
-						// Switch Case because the Data can be either a string or an interface{}
-						// Within this, the tests will be checked, if they were successful or not
-						switch statustype := status.(type) {
-						case string:
-							r, _ := regexp.Compile("does not pass")
-							matcherror := r.MatchString(status.(string))
-							if matcherror {
-								if !matcherr {
-									matcherr = matcherror
-								}
-								fmt.Printf("the test with JobID %d does not succeeded!\n", jobID)
-							}
-							r, _ = regexp.Compile("passes")
-							matchsuccess := r.MatchString(status.(string))
-							if matchsuccess {
-								if !matchsucceed {
-									matchsucceed = matchsuccess
-								}
-								fmt.Printf("the test with JobID %d succeeded!\n", jobID)
-							}
-						case interface{}:
-							for _, item := range status.([]interface{}) {
-								r, _ := regexp.Compile("does not pass")
-								matcherror := r.MatchString(item.(string))
-								if matcherror {
-									if !matcherr {
-										matcherr = matcherror
-									}
-									fmt.Printf("the test with JobID %d does not succeeded!\n", jobID)
-								}
-								r, _ = regexp.Compile("passes")
-								matchsuccess := r.MatchString(item.(string))
-								if matchsuccess {
-									if !matchsucceed {
-										matchsucceed = matchsuccess
-									}
-									fmt.Printf("the test with JobID %d succeeded!\n", jobID)
-								}
-							}
-						// Skip if the data is neither a string nor an interface{}
-						default:
-							fmt.Println(statustype)
-							continue
-						}
-						// Adapt the Github Commit status depending on the jobResults
-						status_desc := jobName + ". Test-Result:"
-						if matcherr {
-							for _, teststatus := range binaryURL {
-								if teststatus.TestName == "push coreboot binary to S3" {
-									var TestStepStatuses = teststatus.TestStepStatuses
-									for _, teststepstatus := range TestStepStatuses {
-										if teststepstatus.TestStepCoordinates.TestStepName == "UploadFile" {
-											var TargetStatuses = teststepstatus.TargetStatuses
-											for _, targetstatus := range TargetStatuses {
-												for _, events := range targetstatus.Events {
-													if events.Data.EventName == "CmdStdout" {
-														var url url
-														var status_desc = jobName + ". Binary in S3 Bucket:"
-														json.Unmarshal(*events.Data.Payload, &url)
-														err := githubAPI.EditGithubStatus(ctx, "error", url.Msg, status_desc, jobSha)
-														if err != nil {
-															fmt.Println("GithubStatus could not be edited to status: error", err)
-														}
-													}
-												}
-											}
-										}
-									}
-								}
-							}
-							err := githubAPI.EditGithubStatus(ctx, "error", fileurl, status_desc, jobSha)
-							if err != nil {
-								fmt.Println("GithubStatus could not be edited to status: error", err)
-							}
-							msg := "Error in commit " + jobSha + ". Something goes wrong in the test with the jobName: " + jobName
-							err = slackAPI.MsgToSlack(msg)
-							if err != nil {
-								fmt.Println("Error could not posted to slack: error", err)
-							}
-						} else if matchsucceed && !matcherr {
-							for _, teststatus := range binaryURL {
-								if teststatus.TestName == "push coreboot binary to S3" {
-									var TestStepStatuses = teststatus.TestStepStatuses
-									for _, teststepstatus := range TestStepStatuses {
-										if teststepstatus.TestStepCoordinates.TestStepName == "UploadFile" {
-											var TargetStatuses = teststepstatus.TargetStatuses
-											for _, targetstatus := range TargetStatuses {
-												for _, events := range targetstatus.Events {
-													if events.Data.EventName == "CmdStdout" {
-														var url url
-														var status_desc = jobName + ". Binary in S3 Bucket:"
-														json.Unmarshal(*events.Data.Payload, &url)
-														err := githubAPI.EditGithubStatus(ctx, "success", url.Msg, status_desc, jobSha)
-														if err != nil {
-															fmt.Println("GithubStatus could not be edited to status: success", err)
-														}
-													}
-												}
-											}
-										}
-									}
-								}
-							}
-							err := githubAPI.EditGithubStatus(ctx, "success", fileurl, status_desc, jobSha)
-							if err != nil {
-								fmt.Println("GithubStatus could not be edited to status: error", err)
-							}
-							msg := "The test with the jobName '" + jobName + "' was successful."
-							err = slackAPI.MsgToSlack(msg)
-							if err != nil {
-								fmt.Println("Error could not posted to slack: error", err)
-							}
-						}
-					}
-				}
-				return nil
-			}
-		} else {
-			fmt.Println("The HTTP Post responded a statuscode != 200")
+		// If API request was not 200 (StatusOK)
+		if resp.StatusCode != http.StatusOK {
+			return fmt.Errorf("the status code of the respone is != 200 (StatusOk)")
 		}
-		// TODO use  time.Ticker instead of time.Sleep
-		// Sleep for the time thats configured in the clientconfig.json
-		time.Sleep(time.Duration(*cd.Flags.FlagjobWaitPoll) * time.Second)
+
+		// Unmarshal the Status of the job that was requested
+		var Status bool
+		bodyBytes, err := ioutil.ReadAll(resp.Body)
+		json.Unmarshal(bodyBytes, &Status)
+		if err != nil {
+			return fmt.Errorf("error reading the HTTP response: %w", err)
+		}
+		// Status contains the job status (true = job finished, false = job still running)
+		// If the job is not already finished
+		if !Status {
+			// Sleep for the time thats configured in the clientconfig.json and than continue
+			time.Sleep(time.Duration(*cd.Flags.FlagjobWaitPoll) * time.Second)
+			continue
+		}
+		// If the job is finished
+		// Than retrieve the jobReport
+		statusResp, err := transport.Status(context.Background(), *cd.Flags.FlagRequestor, types.JobID(jobID))
+		if err != nil {
+			return fmt.Errorf("could not retrieve the jobReport from the server: %w", err)
+		}
+		// Decoding
+		respBodyBytes := new(bytes.Buffer)
+		json.NewEncoder(respBodyBytes).Encode(statusResp)
+
+		// Invoke function that uploads the test report to a S3 Bucket
+		// The function returns the name of the file that was uploaded to use it for the resultURL
+		uploadPath, err := AddFileToS3(s, parameter.s3Path, parameter.s3Bucket, respBodyBytes.Bytes(), jobID)
+		if err != nil {
+			return fmt.Errorf("could upload the jobReport to the S3 bucket: %w", err)
+		}
+
+		// Creating link where the job report can be downloaded.
+		// This link will be put into the commit message right after the test status
+		resultURL := strings.Join([]string{"https://", parameter.s3Bucket, ".s3.", parameter.s3Region, ".amazonaws.com/", uploadPath}, "")
+
+		// Go through the report and retrieve the job status
+		jobStatus = statusResp.Data.Status.JobReport.RunReports
+
+		// Go through all final reports
+		jobSuccess := true
+		for _, allReports := range jobStatus {
+			//Go through all report in the final reports
+			for _, reports := range allReports {
+				var success = reports.Success
+				if !success {
+					jobSuccess = false
+				}
+			}
+		}
+		// Adapt the Github Commit statuses and Slack Msg depending on the job success
+		// Creating the description for the result status and for the binary status
+		resultDesc := jobName + ". Test-Result:"
+		binaryDesc := jobName + ". Binary in S3 Bucket:"
+		// Parse the status resp for the binary url to update the binary status
+		regex := "https://" + parameter.s3Bucket + `[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)`
+		r, _ := regexp.Compile(regex)
+		binaryURL := r.FindString(respBodyBytes.String())
+
+		// If the job was successful
+		if jobSuccess {
+			// Update the binary status
+			err := githubAPI.EditGithubStatus(ctx, "error", binaryURL, binaryDesc, jobSha)
+			if err != nil {
+				fmt.Errorf("githubStatus could not be edited to status 'error': %w", err)
+			}
+			// Update the result status
+			err = githubAPI.EditGithubStatus(ctx, "error", resultURL, resultDesc, jobSha)
+			if err != nil {
+				fmt.Errorf("githubStatus could not be edited to status 'error': %w", err)
+			}
+			// Create a slack msg and than post it
+			msg := strings.Join([]string{"Something goes wrong in the test with the jobName: '", jobName, "'. Commit: '", jobSha, "'."}, "")
+			err = slackAPI.MsgToSlack(msg)
+			if err != nil {
+				fmt.Errorf("error could not posted to slack: error", err)
+			}
+			// If the job errors
+		} else {
+			// Update the binary status
+			err := githubAPI.EditGithubStatus(ctx, "success", binaryURL, binaryDesc, jobSha)
+			if err != nil {
+				fmt.Errorf("githubStatus could not be edited to status 'success': %w", err)
+			}
+			// Update the result status
+			err = githubAPI.EditGithubStatus(ctx, "success", resultURL, resultDesc, jobSha)
+			if err != nil {
+				fmt.Errorf("githubStatus could not be edited to status 'success': %w", err)
+			}
+			// Create a slack msg and than post it
+			msg := strings.Join([]string{"The test with the jobName '", jobName, "' was successful. Commit: '", jobSha, "'."}, "")
+			err = slackAPI.MsgToSlack(msg)
+			if err != nil {
+				fmt.Errorf("could not posted to slack: %w", err)
+			}
+		}
+		return nil
 	}
 }
 
 // AddFileToS3 will upload a single file to S3, it will require a pre-built aws session
 // and will set file info like content type and encryption on the uploaded file.
-func AddFileToS3(s *session.Session, response []byte, jobID int) (string, error) {
+func AddFileToS3(s *session.Session, path string, bucket string, response []byte, jobID int) (string, error) {
 
-	buffer := response
+	// Creating an upload path where the file should be uploaded with a timestamp
 	currentTime := time.Now()
+	uploadPath := strings.Join([]string{path, currentTime.Format("20060102_150405")}, "/")
+	uploadPath = strings.Join([]string{uploadPath, fmt.Sprintf("%v", jobID)}, "_")
 
-	// Config settings: this is where you choose the bucket, filename, content-type etc.
-	// of the file you're uploading.
-	fileName := fmt.Sprintf("%s/%s_%d.json", S3_RESULTS, currentTime.Format("20060102_150405"), jobID)
-
-	//
+	// Uploading the file
 	_, err := s3.New(s).PutObject(&s3.PutObjectInput{
-		Bucket:               aws.String(S3_BUCKET),
-		Key:                  aws.String(fileName),
+		Bucket:               aws.String(bucket),
+		Key:                  aws.String(uploadPath),
 		ACL:                  aws.String("public-read"),
-		Body:                 bytes.NewReader(buffer),
-		ContentLength:        aws.Int64(int64(len(buffer))),
-		ContentType:          aws.String(http.DetectContentType(buffer)),
+		Body:                 bytes.NewReader(response),
+		ContentLength:        aws.Int64(int64(len(response))),
+		ContentType:          aws.String(http.DetectContentType(response)),
 		ContentDisposition:   aws.String("attachment"),
 		ServerSideEncryption: aws.String("AES256"),
 	})
-	fmt.Println("S3 Bucket session established.")
 	if err != nil {
-		return "", err
-	} else {
-		fmt.Printf("Pushed the JobReport from JobID %d to S3 Bucket! \n", jobID)
+		return uploadPath, err
 	}
-	return fileName, err
+
+	// Return the upload path
+	return uploadPath, nil
 }
