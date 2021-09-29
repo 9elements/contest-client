2cd6f06aedf9439dfe4f81dc2559026c48b57b31
llogen <christophlange95@googlemail.com>
2021-09-29 09:52:56 +0200

    coreboot-spr-sp_qemu-boot-test: Added a new test that tests a coreboot boot via qemu. Adapted parts in the code to get the expected result. Adapted the clientconfig to run that test on every incoming webhook and did a minor change on the descriptor yaml layout for better reading.
    Signed-off-by: llogen <christophlange95@googlemail.com>

--- plugins/postjobexecutionhooks/pushtoS3/s3.go
+++ plugins/postjobexecutionhooks/pushtoS3/s3.go
@@ -77,9 +77,20 @@ func PushReportsToS3(ctx context.Context, cd client.ClientDescriptor,
 		regex := "https://" + parameter.S3Bucket + `[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)`
 		r, _ := regexp.Compile(regex)
 		binaryURL := r.FindString(respBodyBytes.String())
+		if binaryURL != "" {
+			// Update the Github status
+			err = UpdateGithubStatus(ctx, jobSuccess, binaryURL, binaryDesc, reportDesc, runData)
+			if err != nil {
+				return err
+			}
+		}
 
 		// Update the Github status
-		err = UpdateGithubStatus(ctx, jobSuccess, binaryURL, reportURL, binaryDesc, reportDesc, runData)
+		err = UpdateGithubStatus(ctx, jobSuccess, reportURL, binaryDesc, reportDesc, runData)
+		if err != nil {
+			return err
+		}
+		err = SendSlackMsg(jobSuccess, runData)
 		if err != nil {
 			return err
 		}
@@ -190,45 +201,49 @@ func CheckJobSuccess(jobStatus [][]*job.Report) bool {
 }
 
 // UpdateGithubStatus updates different Github statuses depending on the success of the job
-func UpdateGithubStatus(ctx context.Context, jobSuccess bool, binaryURL string, reportURL string,
-	binaryDesc string, reportDesc string, runData client.RunData) error {
+func UpdateGithubStatus(ctx context.Context, jobSuccess bool, dataURL string, binaryDesc string,
+	reportDesc string, runData client.RunData) error {
 
 	var Github = clientapi.GithubAPI{}
+
+	// If the job was successful
+	if !jobSuccess {
+		// Update the github status
+		err := Github.EditGithubStatus(ctx, "error", dataURL, binaryDesc, runData.JobSHA)
+		if err != nil {
+			return fmt.Errorf("githubStatus could not be edited to status 'error': %w", err)
+		}
+
+		// If the job errors
+	} else {
+		// Update the github status
+		err := Github.EditGithubStatus(ctx, "success", dataURL, binaryDesc, runData.JobSHA)
+		if err != nil {
+			return fmt.Errorf("githubStatus could not be edited to status 'success': %w", err)
+		}
+	}
+	return nil
+}
+
+// SendSlackMsg sends a msg to slack depending on the success of the job
+func SendSlackMsg(jobSuccess bool, runData client.RunData) error {
+
 	var Slack = clientapi.SlackAPI{}
 
 	// If the job was successful
 	if !jobSuccess {
-		// Update the binary status
-		err := Github.EditGithubStatus(ctx, "error", binaryURL, binaryDesc, runData.JobSHA)
-		if err != nil {
-			return fmt.Errorf("githubStatus could not be edited to status 'error': %w", err)
-		}
-		// Update the report status
-		err = Github.EditGithubStatus(ctx, "error", reportURL, reportDesc, runData.JobSHA)
-		if err != nil {
-			return fmt.Errorf("githubStatus could not be edited to status 'error': %w", err)
-		}
 		// Create a slack msg and than post it
 		msg := strings.Join([]string{"Something goes wrong in the test with the jobName: '", runData.JobName, "'. Commit: '", runData.JobSHA, "'."}, "")
-		err = Slack.MsgToSlack(msg)
+		err := Slack.MsgToSlack(msg)
 		if err != nil {
 			return fmt.Errorf("error could not posted to slack: %w", err)
 		}
+
 		// If the job errors
 	} else {
-		// Update the binary status
-		err := Github.EditGithubStatus(ctx, "success", binaryURL, binaryDesc, runData.JobSHA)
-		if err != nil {
-			return fmt.Errorf("githubStatus could not be edited to status 'success': %w", err)
-		}
-		// Update the report status
-		err = Github.EditGithubStatus(ctx, "success", reportURL, reportDesc, runData.JobSHA)
-		if err != nil {
-			return fmt.Errorf("githubStatus could not be edited to status 'success': %w", err)
-		}
 		// Create a slack msg and than post it
 		msg := strings.Join([]string{"The test with the jobName '", runData.JobName, "' was successful. Commit: '", runData.JobSHA, "'."}, "")
-		err = Slack.MsgToSlack(msg)
+		err := Slack.MsgToSlack(msg)
 		if err != nil {
 			return fmt.Errorf("success could not posted to slack: %w", err)
 		}
